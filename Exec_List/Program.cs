400a655561090b9d76076bc01ad67a2db2950233
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 18:33:21 +0800

    加入專案檔案。

---
+++ Exec_List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_List
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> product = new List<string>() {"1","2","3","4","5","6","7","8","9" };
+		}
+	}
+}
